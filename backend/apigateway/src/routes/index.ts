623ba28288ba1987ed070b0072d1a47f573695e8
Mohamad Aldalati <alda0003@hz.nl>
2025-11-24 19:45:58 +0100

    reset branch

--- backend/apigateway/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import Express, { NextFunction, Request, Response, Router } from 'express';
-const router: Router = Express.Router();
-
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.json('hi');
-  next();
-});
-
-export default router;
